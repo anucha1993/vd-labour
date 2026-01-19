e42c73ffe65afa386e4e6b8c63e5b71b75407ec5
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2026-01-19 13:08:56 +0700

    Update run-scheduler.bat

--- run-scheduler.bat
+++ run-scheduler.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d D:\www\vd-labour
-php artisan joblead:check-notifications >> D:\www\vd-labour\storage\logs\scheduler.log 2>&1
+php artisan schedule:run >> D:\www\vd-labour\storage\logs\scheduler.log 2>&1
